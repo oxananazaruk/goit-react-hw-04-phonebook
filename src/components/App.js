b7135e2424375766356d808a09082a41f1c01a04
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-11-19 14:18:05 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,37 @@
 import { GlobalStyle } from './GlobalStyle';
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 import { Title } from './App.styled';
 
 const storageKey = 'contacts';
+const myContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(myContacts);
+  const [filters, setFilters] = useState('');
 
-  componentDidMount() {
-    const savedContacts = window.localStorage.getItem(storageKey);
+  useEffect(() => {
+    const savedContacts = JSON.parse(window.localStorage.getItem(storageKey));
+
     if (savedContacts !== null) {
-      this.setState({
-        filters: JSON.parse(savedContacts),
-      });
+      setContacts(savedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      window.localStorage.setItem(
-        storageKey,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem(storageKey, JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const addContact = newContact => {
     if (
       contacts.some(
         contact =>
@@ -49,45 +41,35 @@ export class App extends Component {
     ) {
       return Notify.failure(`${newContact.name} is olready in your contacts`);
     }
-    this.setState(prewState => {
-      return {
-        contacts: [...prewState.contacts, { ...newContact, id: nanoid() }],
-      };
+    setContacts(prevContacts => {
+      return [...prevContacts, { ...newContact, id: nanoid() }];
     });
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(item => item.id !== contactId),
-      };
-    });
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  filterContacts = findContact => {
-    this.setState({
-      filter: findContact,
-    });
+  const filterContacts = findContact => {
+    setFilters(findContact);
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const visibleContacts = contacts.filter(contact => {
-      const hasFilter = contact.name
-        .toLocaleLowerCase()
-        .includes(filter.toLocaleLowerCase());
-      return hasFilter;
-    });
-
-    return (
-      <div>
-        <Title>Phonebook</Title>
-        <ContactForm onAdd={this.addContact} />
-        <Title>Contacts</Title>
-        <Filter onFilter={this.filterContacts} />
-        <Contacts items={visibleContacts} onDelete={this.deleteContact} />
-        <GlobalStyle />
-      </div>
+  const visibleContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(filters.toLocaleLowerCase())
     );
-  }
-}
+  };
+
+  const itemContacts = visibleContacts();
+
+  return (
+    <div>
+      <Title>Phonebook</Title>
+      <ContactForm onAdd={addContact} />
+      <Title>Contacts</Title>
+      <Filter onFilter={filterContacts} />
+      <Contacts items={itemContacts} onDelete={deleteContact} />
+      <GlobalStyle />
+    </div>
+  );
+};
